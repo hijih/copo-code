fc40d81496f88a42f251b84a24e941666f8899d4
hiijh <1733904771@qq.com>
2025-05-21 17:08:13 +0800

    onlyGloal0521

--- verl/run1.sh
+++ verl/run1.sh
@@ -38,8 +38,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
-    actor_rollout_ref.actor.use_kl_loss=false \
-    actor_rollout_ref.actor.kl_loss_coef=0 \
+    actor_rollout_ref.actor.use_kl_loss=true \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
